2a750733719f56d54cc46af99b19d5e4e373d108
AmineYoussef <amine.youssef@xevlabs.com>
2022-12-11 16:09:39 +0100

    ajout accueil + nettoyage css + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,21 @@ import { UpdateHComponent } from './ProductH/update-h/update-h.component';
 import { List_hComponent } from './ProductH/list-h/list-h.component';
 import { UpdateEComponent } from './ProductE/update-e/update-e.component';
 import { List_eComponent } from './ProductE/list-e/list-e.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 
 
 
 
 const routes: Routes = [
-  {path : 'ProduitF' , component : ListComponent},
-  {path : 'updateF/:id' , component : UpdateFComponent},
-  {path : 'ProduitH' , component :List_hComponent },
-  {path : 'updateH/:id' , component : UpdateHComponent},
-  {path : 'ProduitE' , component :List_eComponent },
-  {path : 'updateE/:id' , component : UpdateEComponent},
-
+  { path: 'ProduitF', component: ListComponent },
+  { path: 'updateF/:id', component: UpdateFComponent },
+  { path: 'ProduitH', component: List_hComponent },
+  { path: 'updateH/:id', component: UpdateHComponent },
+  { path: 'ProduitE', component: List_eComponent },
+  { path: 'updateE/:id', component: UpdateEComponent },
+  { path: 'accueil', component: AccueilComponent },
+  { path: '**', redirectTo: 'accueil' },
 ];
 
 @NgModule({
